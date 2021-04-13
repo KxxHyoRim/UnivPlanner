27627f5696181784fde06b9edf4f33ac084c5953
KxxHyoRim <rlagyfla0709@gmail.com>
2021-04-14 02:34:55 +0900

    Edit AndroidManifest.xml(add activity)

--- app/src/main/java/edu/sungshin/univplanner/login.java
+++ app/src/main/java/edu/sungshin/univplanner/login.java
@@ -3,7 +3,6 @@ package edu.sungshin.univplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.WindowManager;
 
 public class login extends AppCompatActivity {
 
