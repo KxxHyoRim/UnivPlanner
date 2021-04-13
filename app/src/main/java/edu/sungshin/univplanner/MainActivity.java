dbf2e14e3581d5273693a86e218f829dbf19f7ee
KxxHyoRim <rlagyfla0709@gmail.com>
2021-04-14 02:21:13 +0900

    Remove Actionbar on login page

--- app/src/main/java/edu/sungshin/univplanner/MainActivity.java
+++ app/src/main/java/edu/sungshin/univplanner/MainActivity.java
@@ -9,6 +9,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 주석해제하면 상태바 없어짐(로그인화면)
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.main);
     }
 }
---
+++ app/src/main/java/login.java
@@ -0,0 +1,16 @@
+package edu.sungshin.univplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.login);
+    }
+}
