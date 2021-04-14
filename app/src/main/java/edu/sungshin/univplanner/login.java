f8a7cea5a3a77c26648e461e361a1dc3a9d3c704
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2021-04-14 20:43:50 +0900

    login.xml add signup TextView

--- app/src/main/java/edu/sungshin/univplanner/login.java
+++ app/src/main/java/edu/sungshin/univplanner/login.java
@@ -2,14 +2,27 @@ package edu.sungshin.univplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
+    TextView signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.login);
+
+        signup = (TextView) findViewById(R.id.signup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
