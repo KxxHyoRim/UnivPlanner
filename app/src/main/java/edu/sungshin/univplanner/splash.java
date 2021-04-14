395910164c64efa6a046b129b3a41bc62812f714
KxxHyoRim <rlagyfla0709@gmail.com>
2021-04-14 20:15:06 +0900

    Add splash

---
+++ app/src/main/java/edu/sungshin/univplanner/splash.java
@@ -0,0 +1,26 @@
+package edu.sungshin.univplanner;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try{
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Intent intent = new Intent(this, login.class);
+        intent.putExtra("state", "launch");
+
+        startActivity(intent);
+        finish();
+    }
+}
