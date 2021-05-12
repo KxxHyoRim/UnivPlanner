0ebca635eee4cc679c28339d75e1bb8b53e1c07e
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2021-05-12 22:25:09 +0900

    setting + profile

--- app/src/main/java/edu/sungshin/univplanner/SettingActivity.java
+++ app/src/main/java/edu/sungshin/univplanner/SettingActivity.java
@@ -1,15 +1,105 @@
 package edu.sungshin.univplanner;
 
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 
 public class SettingActivity extends AppCompatActivity {
 
+    final int GET_GALLERY_IMAGE = 200;
+    ImageView imageView;
+    String imgName = "profile.png";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        try {
+            String imgpath = getCacheDir() + "/" + imgName;   // 내부 저장소에 저장되어 있는 이미지 경로
+            Bitmap bm = BitmapFactory.decodeFile(imgpath);
+            imageView.setImageBitmap(bm);   // 내부 저장소에 저장된 이미지를 이미지뷰에 셋
+            if(bm == null)
+                imageView.setImageResource(R.drawable.profileimage);
+        } catch (Exception e) {
+        }
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String[] versionArray = new String[] { "프로밀 이미지 삭제", "프로필 이미지 변경"};
+                AlertDialog.Builder dlg = new AlertDialog.Builder(SettingActivity.this);
+                dlg.setItems(versionArray,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                if(which == 0){
+                                    try {
+                                        File file = getCacheDir();  // 내부저장소 캐시 경로를 받아오기
+                                        File[] flist = file.listFiles();
+                                        for (int i = 0; i < flist.length; i++) {    // 배열의 크기만큼 반복
+                                            if (flist[i].getName().equals(imgName)) {   // 삭제하고자 하는 이름과 같은 파일명이 있으면 실행
+                                                flist[i].delete();  // 파일 삭제
+                                            }
+                                        }
+                                    } catch (Exception e) {
+                                    }
+                                    imageView.setImageResource(R.drawable.profileimage);
+                                }
+                                else if(which==1){
+                                    Intent intent = new Intent();
+                                    intent.setType("image/*");
+                                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                                    startActivityForResult(intent, GET_GALLERY_IMAGE);
+                                }
+                            }
+                        });
+                dlg.show();
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GET_GALLERY_IMAGE) {
+            if (resultCode == RESULT_OK) {
+                Uri fileUri = data.getData();
+                ContentResolver resolver = getContentResolver();
+                try {
+                    InputStream instream = resolver.openInputStream(fileUri);
+                    Bitmap imgBitmap = BitmapFactory.decodeStream(instream);
+                    imageView.setImageBitmap(imgBitmap);    // 선택한 이미지 이미지뷰에 셋
+                    instream.close();   // 스트림 닫아주기
+                    saveBitmapToJpeg(imgBitmap);    // 내부 저장소에 저장
+                } catch (Exception e) {
+                }
+
+            }
+        }
+
+    }
+    public void saveBitmapToJpeg(Bitmap bitmap) {   // 선택한 이미지 내부 저장소에 저장
+        File tempFile = new File(getCacheDir(), imgName);    // 파일 경로와 이름 넣기
+        try {
+            tempFile.createNewFile();   // 자동으로 빈 파일을 생성하기
+            FileOutputStream out = new FileOutputStream(tempFile);  // 파일을 쓸 수 있는 스트림을 준비하기
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);   // compress 함수를 사용해 스트림에 비트맵을 저장하기
+            out.close();    // 스트림 닫아주기
+        } catch (Exception e) {
+        }
     }
 }
