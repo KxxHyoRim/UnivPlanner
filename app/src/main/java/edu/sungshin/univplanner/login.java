208d1e39ba2e548706a99613007f80be07677cb4
ghdus819 <ghdus819@gmail.com>
2021-05-23 08:05:40 +0900

    Add Login with Server

--- app/src/main/java/edu/sungshin/univplanner/login.java
+++ app/src/main/java/edu/sungshin/univplanner/login.java
@@ -4,28 +4,80 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class login extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
-    Button login;
-    TextView signup;
+public class login extends AppCompatActivity {
+    Button button;
+    EditText idEditText, pwEditText;
+    String idText, pwText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        login = (Button) findViewById(R.id.login_btn);
-        login.setOnClickListener(new View.OnClickListener() {
+        button = (Button) findViewById(R.id.login_btn);
+        idEditText = (EditText) findViewById(R.id.login_id);
+        pwEditText = (EditText) findViewById(R.id.login_pw);
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                idText = idEditText.getText().toString();
+                pwText = pwEditText.getText().toString();
+                Log.e("btn", "click");
+                ClientThread thread = new ClientThread();
+                thread.start();
             }
         });
+    }
 
+    protected class ClientThread extends Thread {
+        public void run() {
+            String host = "ec2-13-209-76-12.ap-northeast-2.compute.amazonaws.com";
+            int port = 8080;
 
+            try {
+                Log.e("sck", "start");
+                Socket socket = new Socket(host, port);
+                Log.e("sck", "suc");
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+                out.println(idText);
+                Log.e("send", idText);
+
+                out.println(pwText);
+                //Log.e("send", pwText);
+
+                String rev = in.readLine();
+                Log.e("receive", rev);
+
+                if (rev.equals("Success")) {
+                    Log.e("receive", "Success");
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+
+                else {
+                    Log.e("receive", "failed");
+                }
+            }
+
+            catch (Exception e) {
+                Log.e("sck", "fail");
+                e.printStackTrace();
+            }
+        }
     }
 }
