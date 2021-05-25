209537766dfde6bd6c3f39c68ca35bb65e52d53a
KxxHyoRim <rlagyfla0709@gmail.com>
2021-05-25 23:18:21 +0900

    setting - preference screen 개인별 과목 설정

--- app/src/main/java/edu/sungshin/univplanner/Setting.java
+++ app/src/main/java/edu/sungshin/univplanner/Setting.java
@@ -40,12 +40,14 @@ public class Setting extends PreferenceFragment {
     private ChildEventListener myChildEventListener;
     private FirebaseAuth mAuth;
 
-    ListView lecture_list;
-    ListViewAdapter listview_adapter;
     String lecture_fullList;
     int totalLectureNum;
     String[] lectureName_array;
+    String lecture_key;
+    String assignment_key;
 
+
+    PreferenceScreen rootPreference;
     CheckBoxPreference subjPreference;
     PreferenceScreen lectureListScreen;
     PreferenceScreen assignmentListScreen;
@@ -60,12 +62,10 @@ public class Setting extends PreferenceFragment {
 
         lectureListScreen = (PreferenceScreen)findPreference("optional_subject_lecture_in");
 
-        subjPreference = (CheckBoxPreference)findPreference("lecture_subject1");
-        String tmp = (String) subjPreference.getTitle();
-        Log.e("preference_get", tmp + "");
 
 
-        /*--------------크롤링 데이터 DB에서 가져오기-------------------*/
+
+        /** firebase 연동 */
 
         //로그인한 유저의 정보 가져오기
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -73,26 +73,63 @@ public class Setting extends PreferenceFragment {
         String userInfo = user.getUid();
         Log.e("fb uid in setting", userInfo);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance("https://univp-1db5d-default-rtdb.asia-southeast1.firebasedatabase.app/");
-        DatabaseReference myRef = database.getReference("User").child(userInfo).child("lectureName");
+        FirebaseDatabase database = FirebaseDatabase.getInstance(
+                "https://univp-1db5d-default-rtdb.asia-southeast1.firebasedatabase.app/");
+
+        DatabaseReference myRef = database.getReference("User").
+                child(userInfo).child("lectureName");
+
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
 
                 lecture_fullList = snapshot.getValue(String.class);
-                //Log.e("load letureName:", lecture_fullList + "");
 
+                // 수강하는 과목 개수 가져오기
                 lectureName_array = lecture_fullList.split("\n");
                 totalLectureNum = Integer.parseInt(lectureName_array[0]);
                 Log.e("total_lecture_num", totalLectureNum + "");
 
 
+                // 수강과목 과목명 가져오기
                 for(int i=1; i<totalLectureNum+1;i++){
                     String lectureName = lectureName_array[i];
                     Log.e("lecure_name_setting", lectureName + "");
                 }
 
-                subjPreference.setTitle(lectureName_array[1]);
+                String lecture_base_key = "lecture_subject";
+                String assignment_base_key = "assignment_subject";
+
+                // 과목 이름 설정
+                for(int i=1; i<totalLectureNum+1;i++){
+
+                    // 강의
+                    lecture_key = lecture_base_key + i;
+                    subjPreference = (CheckBoxPreference)findPreference(lecture_key);
+                    subjPreference.setTitle(lectureName_array[i]);
+
+                    //과제
+                    assignment_key = assignment_base_key + i;
+                    subjPreference = (CheckBoxPreference)findPreference(assignment_key);
+                    subjPreference.setTitle(lectureName_array[i]);
+
+                }
+
+                // CheckBoxPreference 삭제하는 코드
+                for (int i = 8; i > totalLectureNum; i--){
+                    lecture_key = lecture_base_key + i;
+                    rootPreference = (PreferenceScreen)findPreference("optional_subject_lecture");
+                    subjPreference = (CheckBoxPreference)findPreference(lecture_key);
+                    rootPreference.removePreference(subjPreference);
+
+                    assignment_key = assignment_base_key + i;
+                    rootPreference = (PreferenceScreen)findPreference("optional_subject_assignment");
+                    subjPreference = (CheckBoxPreference)findPreference(assignment_key);
+                    rootPreference.removePreference(subjPreference);
+
+                }
+
+
 
 
 
@@ -102,6 +139,7 @@ public class Setting extends PreferenceFragment {
             @Override
             public void onCancelled(@NonNull DatabaseError error) { }
         });
+
 
 
 
